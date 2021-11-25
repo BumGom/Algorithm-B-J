423526b09e4bc4862f8f60b7467617d1aac7e553
Junhobu <68681792+Junhobu@users.noreply.github.com>
2021-11-26 01:24:51 +0900

    액션바에서 개발자 및 참고문헌 페이지 연결 완료 이젠 진짜 끝이다 시발

---
+++ app/src/main/java/extrapage/reference.java
@@ -0,0 +1,16 @@
+package extrapage;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.algorithm_bj.R;
+
+public class reference extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extrapage_reference);
+    }
+}
