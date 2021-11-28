008a0a8042fc015ac3999b45df620dcd9bf375a1
BumGom <jjh33055@naver.com>
2021-11-28 23:59:56 +0900

    퀵정렬 했다

--- app/src/main/java/explainpage/sort/quicksort.java
+++ app/src/main/java/explainpage/sort/quicksort.java
@@ -3,11 +3,15 @@ package explainpage.sort;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.algorithm_bj.R;
 
 public class quicksort extends AppCompatActivity {
@@ -20,6 +24,13 @@ public class quicksort extends AppCompatActivity {
         setContentView(R.layout.activity_explainpage_5_3_quicksort);
 
         txtResult = (TextView) findViewById(R.id.textResult);
+
+        // GIF add code +
+        ImageView quicksort = (ImageView) findViewById(R.id.quicksortgifexplain);
+        Glide.with(this).load(R.raw.quicksortfigraw)
+                .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
+                .into(quicksort);
+        // GIF add code -
     }
 
     public void mOnPopupClick(View v) {
