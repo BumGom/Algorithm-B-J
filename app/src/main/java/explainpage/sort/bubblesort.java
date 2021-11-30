7b0dbfa7f97904019694a421b41241d06ca7a454
BumGom <jjh33055@naver.com>
2021-11-30 17:27:46 +0900

    정렬 완료

--- app/src/main/java/explainpage/sort/bubblesort.java
+++ app/src/main/java/explainpage/sort/bubblesort.java
@@ -3,11 +3,15 @@ package explainpage.sort;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.algorithm_bj.R;
 
 public class bubblesort extends AppCompatActivity {
@@ -20,6 +24,13 @@ public class bubblesort extends AppCompatActivity {
         setContentView(R.layout.activity_explainpage_5_1_bubblesort);
 
         txtResult = (TextView) findViewById(R.id.textResult);
+
+        ImageView bubblesort = (ImageView) findViewById(R.id.bubblesortgifexplain);
+
+        Glide.with(this).load(R.raw.bubblesortgifraw)
+                .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
+                .into(bubblesort);
+        // GIF add code -
     }
 
     public void mOnPopupClick(View v) {
--- app/src/main/java/explainpage/sort/insertionsort.java
+++ app/src/main/java/explainpage/sort/insertionsort.java
@@ -3,11 +3,15 @@ package explainpage.sort;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.algorithm_bj.R;
 
 public class insertionsort extends AppCompatActivity {
@@ -20,6 +24,12 @@ public class insertionsort extends AppCompatActivity {
         setContentView(R.layout.activity_explainpage_5_2_insertionsort);
 
         txtResult = (TextView) findViewById(R.id.textResult);
+        ImageView insertionsort = (ImageView) findViewById(R.id.insertionsortgifexplain);
+
+        Glide.with(this).load(R.raw.insertsortgifraw)
+                .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
+                .into(insertionsort);
+        // GIF add code -
     }
 
     public void mOnPopupClick(View v) {
